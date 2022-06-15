948e0790e5f850fac63b0962dd7ca6470cb46848
Gabriel <gabrielcorena@gmail.com>
2022-06-15 14:03:19 -0600

    probando smtp outlook

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,10 +44,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.live.com",
     :port                 => 587,
-    :user_name            => "la.nueva.empresa.bad@gmail.com",
-    :password             => "2pupusas",
+    :user_name            => "solucionesdma@outlook.com",
+    :password             => "solucionesdm@",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
